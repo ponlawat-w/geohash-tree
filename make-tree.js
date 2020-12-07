3eeb35666fd90a2660ce2ce6bb7c949435733d9a
ponlawat-w <ponlawat_w@cmu.ac.th>
2020-12-07 10:40:38 +0100

    Fix Code Order

--- make-tree.js
+++ make-tree.js
@@ -1,11 +1,11 @@
 const travel = (geohash, tree) => {
   const char = geohash[0];
-  if (!tree[char]) {
-    tree[char] = {};
-  }
   if (geohash.length === 1) {
     tree[char] = 1;
     return;
+  }
+  if (!tree[char]) {
+    tree[char] = {};
   }
   travel(geohash.slice(1), tree[char]);
 };
